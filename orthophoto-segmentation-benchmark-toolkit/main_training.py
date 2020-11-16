1340e6f055ef3410d7aca64520ccf5e3dbf65175
Arne Schulz <arne.schulz@tu-bs.de>
2020-11-16 10:53:31 +0100

    added batch_size argument to train cli

--- orthophoto-segmentation-benchmark-toolkit/main_training.py
+++ orthophoto-segmentation-benchmark-toolkit/main_training.py
@@ -11,6 +11,7 @@ enable_dynamic_memory_growth()
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", help="model weights of given eperiment will be used for training", default=30)
+    parser.add_argument("--bs", help="model weights of given eperiment will be used for training", default=4)
     parser.add_argument("--experiment", help="model weights of given eperiment will be used for training", default="")
     args = parser.parse_args()
 
@@ -21,7 +22,7 @@ if __name__ == '__main__':
     dataset = DroneDeployDataset(dataset, size).download().generate_chips()
     model_backend = UnetBackend('efficientnetb3')
 
-    experiment = Experiment("test", dataset, model_backend, batch_size=1,
+    experiment = Experiment("test", dataset, model_backend, batch_size=args.bs,
                             experiment_directory=args.experiment, load_best=False)
     experiment.analyze()
     experiment.train(epochs=args.epochs)
